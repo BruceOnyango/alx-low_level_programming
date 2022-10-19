6aa42bb597ef7adbc867d37645337c66cd48a5c8
Your Name <you@example.com>
2022-10-19 05:30:24 -0700

    100-times_table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,73 @@
 #include "main.h"
+
 #include "6-abs.c"
+
 #include <stdio.h>
 
+
+
 /**
+
  * print - show numbers till 98
+
  * @n: number to begin from
+
  *
+
  * Return: numbers upto 98
+
  */
+
 void print_to_98(int n)
+
 {
+
 	int i;
 
+
+
 	if (n > 98)
+
 	{
+
 		for (i = n; i >= 98; i--)
+
 		{
+
 			printf("%d", i);
+
 			if (i != 98)
+
 			{
+
 				printf(", ");
+
 			}
+
 		}
+
 	} else
+
 	{
+
 		for (i = n; i <= 98; i++)
+
 		{
+
 			printf("%d", i);
+
 			if (i != 98)
+
 			{
+
 				printf(", ");
+
 			}
+
 		}
+
 	}
+
 	printf("\n");
+
 }
