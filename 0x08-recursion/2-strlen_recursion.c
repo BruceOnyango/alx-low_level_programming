411255c29517155a7f395cd229d179fe1a024bcf
BruceOnyango <bruce.onyango@strathmore.edu>
2022-11-03 19:22:37 -0700

    changes

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,78 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s: string to count
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
 
-
-/*                                                                                                                                      
- * File: 2-strlen_recursion.c                                                                                                           
- * Auth: smone                                                                                                                          
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _strlen_recursion - Returns the length of a string.                                                                                  
- * @s: The string to be measured.                                                                                                       
- *                                                                                                                                      
- * Return: The length of the string.                                                                                                    
- */                                                                                                                                     
-int _strlen_recursion(char *s)                                                                                                          
-{                                                                                                                                       
-        int len = 0;                                                                                                                    
-                                                                                                                                        
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                len++;                                                                                                                  
-                len += _strlen_recursion(s + 1);                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        return (len);                                                                                                                   
+	if (*s)
+	{
+		n = _strlen_recursion(s + 1);
+		return (n += 1);
+	}
+	return (0);
 }
-/*                                                                                                                                      
-
- * File: 2-strlen_recursion.c                                                                                                           
-
- * Auth: smone                                                                                                                          
-
- */                                                                                                                                     
-
-                                                                                                                                        
-
-#include "main.h"                                                                                                                       
-
-                                                                                                                                        
-
-/**                                                                                                                                     
-
- * _strlen_recursion - Returns the length of a string.                                                                                  
-
- * @s: The string to be measured.                                                                                                       
-
- *                                                                                                                                      
-
- * Return: The length of the string.                                                                                                    
-
- */                                                                                                                                     
-
-int _strlen_recursion(char *s)                                                                                                          
-
-{                                                                                                                                       
-
-        int len = 0;                                                                                                                    
-
-                                                                                                                                        
-
-        if (*s)                                                                                                                         
-
-        {                                                                                                                               
-
-                len++;                                                                                                                  
-
-                len += _strlen_recursion(s + 1);                                                                                        
-
-        }                                                                                                                               
-
-                                                                                                                                        
-
-        return (len);                                                                                                                   
-
-}
-
-
