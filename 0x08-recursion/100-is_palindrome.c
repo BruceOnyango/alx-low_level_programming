c098bd957c112f5780b6fc3ecc44ca45bcd4f518
BruceOnyango <bruce.onyango@strathmore.edu>
2022-11-03 19:32:29 -0700

    changes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,58 +1,50 @@
-/*                                                                                                                                    
+#include "main.h"
+/**
+ * length - helps do it
+ * @s: string
+ * Return: string length
+ */
+int length(char *s)
+{
+	int i = 0;
 
- * File: 100-is_palindrome.c                                                                                                          
+	if (*s)
+	{
+		i = i + length(s + 1);
+		return (i += 1);
+	}
+	return (0);
+}
+/**
+ * helper2 - helps more
+ * @i: integer i
+ * @s: string
+ * Return: int value
+ */
+int helper2(int i, char *s)
+{
+	if (*s)
+	{
+		if (*s != s[length(s) - i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (helper2(i + 1, s + 1));
+		}
+	}
+	return (1);
+}
+/**
+ * is_palindrome - is it paldindrome
+ * @s: string to check
+ * Return: boolean
+ */
+int is_palindrome(char *s)
+{
+	int i = 1;
 
- * Auth: sami                                                                                                                         
-
- */                                                                                                                                   
-
-                                                                                                                                      
-
-#include "main.h"                                                                                                                     
-
-                                                                                                                                      
-
-int find_strlen(char *s);                                                                                                             
-
-int check_palindrome(char *s, int len, int index);                                                                                    
-
-int is_palindrome(char *s);                                                                                                           
-
-                                                                                                                                      
-
-/**                                                                                                                                   
-
- * find_strlen - Returns the length of a string.                                                                                      
-
- * @s: The string to be measured.                                                                                                     
-
- *                                                                                                                                    
-
- * Return: The length of the string.                                                                                                  
-
- */                                                                                                                                   
-
-int find_strlen(char *s)                                                                                                              
-
-{                                                                                                                                     
-
-        int len = 0;                                                                                                                  
-
-                                                                                                                                      
-
-        if (*(s + len))                                                                                                               
-
-        {                                                                                                                             
-
-                len++;                                                                                                                
-
-                len += find_strlen(s + len);                                                                                          
-
-        }                                                                                                                             
-
-                                                                                                                                      
-
-        return (len);                                                                                                                 
+	return (helper2(i, s));
 
 }
-
